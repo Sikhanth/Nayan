0ea5f359ee3eafebb7abe70db44e91d32b3d0c31
Sikhanth SV <sikhanth10@gmail.com>
2026-09-15 11:11:54 +0530

    Fix hardcoded localhost URLs in authService and detectionService

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://127.0.0.1:8000/users";
+const API_URL = `${import.meta.env.VITE_API_URL}/users`;
 
 export const loginUser = async (username, password) => {
   const formData = new URLSearchParams();
